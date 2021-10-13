7232ffdbf9215a255e41ab6b98ede718b2211ff8
Sta11k <Alexandiel1984@gmail.com>
2021-10-13 15:11:02 +0300

    startpart2

--- src/components/phonebook/phonebook.js
+++ src/components/phonebook/phonebook.js
@@ -1,76 +1,112 @@
 import React from 'react';
 import { Component } from 'react';
-import styles from './phonebook.module.css';
+import s from './phonebook.module.css';
+import { v4 as uuid } from 'uuid';
 
-const Add = ({ changeMessage, label }) => (
-  <button type="button" onClick={changeMessage}>
-    {label}
-  </button>
-);
+// const Add = ({ changeMessage, label }) => (
+//   <button type="button" onClick={changeMessage}>
+//     {label}
+//   </button>
+// );
 
 class Phonebook extends Component {
   state = {
-    contacts: [],
+    contactsPhone: null,
+    contactsOll: [],
     filter: '',
     name: '',
     number: '',
   };
 
-  updateMessage = evt => {
-    console.log(evt);
+  // updateMessage = evt => {
+  //   console.log(evt);
 
+  //   this.setState({
+  //     contacts: [],
+  //     filter: '',
+  //     name: '',
+  //     number: '',
+  //   });
+  // };
+  hendleChange = e => {
+    // console.log(e.target.name, e.target.value);
     this.setState({
-      contacts: [],
-      filter: '',
-      name: '',
-      number: '',
+      [e.target.name]: e.target.value,
     });
   };
 
+  hendleSubmit = e => {
+    e.preventDefault();
+    const contactsObj = {
+      name: this.state.name,
+      number: this.state.number,
+    };
+    this.setState({
+      contactsPhone: contactsObj,
+    });
+    this.setState(prevState => {
+      return { contactsOll: [...prevState.contactsOll, contactsObj] };
+    });
+    this.reserForm();
+  };
+
+  reserForm = () => {
+    this.setState({ name: ' ', number: ' ' });
+  };
+  contactIdName = uuid();
+  contactIdNumber = uuid();
+
   render() {
+    console.log(this.state.name, this.state.number);
     return (
       <>
-        <h1>Phonebook</h1>
-        <div className={styles.addBlock}>
-          <label className={styles.name}>
-            Name
-            <input
-              type="tel"
-              name="name"
-              // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="ім'я"
-              required
-            />
-          </label>
-          <label className={styles.name}>
-            Number
-            <input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-              required
-            />
-          </label>
+        <form onSubmit={this.hendleSubmit}>
+          <h1>Phonebook</h1>
+          <div className={s.addBlock}>
+            <label htmlFor={this.contactIdName} className={s.name}>
+              Name
+              <input
+                id={this.contactIdName}
+                onChange={this.hendleChange}
+                type="text"
+                value={this.state.name}
+                name="name"
+                title="ім'я"
+                required
+              />
+            </label>
+            <label htmlFor={this.contactIdNumber} className={s.name}>
+              Number
+              <input
+                id={this.contactIdNumber}
+                onChange={this.hendleChange}
+                type="tel"
+                value={this.state.number}
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+                required
+              />
+            </label>
+            <button type="submit">Add</button>
+            {/* <Add label="Add contacts" changeMessage={this.updateMessage} /> */}
+          </div>
+        </form>
 
-          <Add label="Add contacts" changeMessage={this.updateMessage} />
-        </div>
-
-        <div className={styles.search}>
+        <div className={s.search}>
           <h2>Contacts</h2>
-          <label className={styles.name}>
-            Find contacts by name
-            <input
-              type="tel"
-              name="search"
-              // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Пошук контактів за ім'ям"
-              required
-            />
-          </label>
+          {/* <label className={s.name}>
+              Find contacts by name
+              <input
+                type="tel"
+                name="search"
+               
+                title="Пошук контактів за ім'ям"
+               />
+            </label> */}
+
           <ul className="contacts__list"></ul>
         </div>
-
         {/* <div className={styles.result}>
           <span>No feedback given</span>
           <span>Good: {this.state.good}</span>
